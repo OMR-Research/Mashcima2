a4a126b4dc83891cb26aaeb0dde9ef51c05e2404
Jirka Mayer <jirkamayer97@gmail.com>
2024-09-16 19:17:47 +0200

    Assets layer docs

--- mashcima2/config.py
+++ mashcima2/config.py
@@ -7,7 +7,7 @@ from pathlib import Path
 DEFAULT_XDG_CACHE_HOME = "~/.cache"
 XDG_CACHE_HOME = os.getenv("XDG_CACHE_HOME", DEFAULT_XDG_CACHE_HOME)
 DEFAULT_MC_CACHE_HOME = os.path.join(XDG_CACHE_HOME, "mashcima2")
-MC_CACHE_HOME = os.path.expanduser(os.getenv("HF_HOME", DEFAULT_MC_CACHE_HOME))
+MC_CACHE_HOME = os.path.expanduser(os.getenv("MC_HOME", DEFAULT_MC_CACHE_HOME))
 
 # default asset repository path
 DEFAULT_MC_ASSETS_CACHE = os.path.join(MC_CACHE_HOME, "assets")
